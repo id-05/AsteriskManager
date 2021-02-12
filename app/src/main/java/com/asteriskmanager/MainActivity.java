93005c896e6c7b7b61e505f929a44738304b9af4
id-05 <id-05@mail.ru>
2021-02-13 02:06:30 +0900

    toptop place

--- app/src/main/java/com/asteriskmanager/MainActivity.java
+++ app/src/main/java/com/asteriskmanager/MainActivity.java
@@ -2,6 +2,7 @@ package com.asteriskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -28,17 +29,21 @@ import ch.loway.oss.ari4java.ARI;
 import ch.loway.oss.ari4java.AriVersion;
 import ch.loway.oss.ari4java.generated.models.Application;
 import ch.loway.oss.ari4java.generated.models.AsteriskInfo;
+import ch.loway.oss.ari4java.generated.models.Bridge;
 import ch.loway.oss.ari4java.generated.models.Channel;
+import ch.loway.oss.ari4java.generated.models.DeviceState;
+import ch.loway.oss.ari4java.generated.models.Endpoint;
 import ch.loway.oss.ari4java.generated.models.Module;
 import ch.loway.oss.ari4java.tools.ARIException;
 import ch.loway.oss.ari4java.tools.RestException;
 
 import static java.lang.Thread.sleep;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ConnectionCallback {
 
     Button but1,but2;
     //public static ManagerConnection managerConnection;
+    ARI ari = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,28 +59,30 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener newclick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            ARI ari = null;
-            try {
-                ari = ARI.build("http://188.75.221.200:30888"/*"http://188.75.221.200:30888/"*/, "test-app", "restuser", "pr09ramm1$t", AriVersion.IM_FEELING_LUCKY);
-                //String s = "";
-                List<Channel> channels = ari.channels().list().execute();
-                for(Channel chan:channels){
-                    Log.d("aster","chan.getName() = "+chan.getName().toString());
-                }
-               // ari.get
-                ari.applications().list().execute();
-                List<Application> lm = ari.applications().list().execute();
-                for(Application lmodule:lm){
-                    Log.d("aster","Application = "+lmodule.getName() );
 
-                }
+            try {
+                Log.d("aster","aster");
+               ari = ARI.build("http://188.75.221.200:30888"/*"http://188.75.221.200:30888/"*/, "test-app", "restuser", "pr09ramm1$t", AriVersion.IM_FEELING_LUCKY);
+//                List<Channel> channels = ari.channels().list().execute();
+//                for(Channel chan:channels){
+//                    Log.d("aster","chan.getName() = "+chan.getName().toString());
+//                }
+
+                doSomethingAsyncOperaion("!", "!");
+               // ari.get
+//                ari.applications().list().execute();
+//                List<Application> lm = ari.applications().list().execute();
+//                for(Application lmodule:lm){
+//                    Log.d("aster","Application = "+lmodule.getName() );
+//
+//                }
                // ApplicationList applicationList=ari.applications().list().execute();
 
-               Channel chan1 = ari.channels().originate("Local/89145077248@from-internal")
-                       .setExtension("334485").setContext("from-internal").setPriority(1).setCallerId("89145077248").execute();
+//               Channel chan1 = ari.channels().originate("Local/89145077248@from-internal")
+//                       .setExtension("334485").setContext("from-internal").setPriority(1).setCallerId("89145077248").execute();
 
           //      Log.d("aster","Channel:" + chan1.getId() + " in state " + chan1.getState());
-            } catch (ARIException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
@@ -128,4 +135,53 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+
+    @SuppressLint("StaticFieldLeak")
+    public void doSomethingAsyncOperaion(final String comand, final String number) {
+        new AbstractAsyncWorker<Boolean>(this, comand,number) {
+            //@SuppressLint("StaticFieldLeak")
+            @SuppressLint("StaticFieldLeak")
+            @Override
+            protected Boolean doAction() throws Exception {
+
+                List<Endpoint> DevSt = ari.endpoints().list().execute();
+                //interface Peer
+                //List<Application> lm = ari.applications().list().execute();
+                for(Endpoint lmodule:   DevSt){
+                    Log.d("aster","endpoints() = "+lmodule.getResource().toString() +"   endpoints() state= "+lmodule.getState() );
+
+                   //Log.d("aster","endpoints() state= "+lmodule.getState() );
+
+                }
+
+                ari.
+                List<Bridge> brlist = ari.bridges().list().execute();
+                for(Bridge br: brlist){
+                    Log.d("aster","brigh  = "+br.getName());
+                }
+                return true;
+            }
+        }.execute();
+    }
+
+    @Override
+    public void onBegin() {
+
+    }
+
+    @Override
+    public void onSuccess(String data, String param) {
+
+    }
+
+    @Override
+    public void onFailure(Throwable t) {
+
+    }
+
+    @Override
+    public void onEnd() {
+
+    }
 }
